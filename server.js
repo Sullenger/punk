1bd95d8763a4e258ca14fef2cdcb18867c87b2a0
Jason Sullenger <jssullenger@my365.bellevue.edu>
2021-01-14 22:21:07 -0700

    Heroku ts

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + './dist'));
+app.use(express.static(__dirname + './dist/rw-punk-challenge'));
 
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/index.html'));});
+'/dist/rw-punk-challenge/index.html'));});
 
 app.listen(process.env.PORT || 8080);
